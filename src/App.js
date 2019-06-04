a4a9bd800f5a2153aa02c4b99f4fc88cdf83f7ea
Martin Prieur <martin.prieur@outlook.fr>
2019-06-04 17:20:30 +0200

    Display a clickable keyboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class App extends Component {
+import BoardKey from './BoardKey';
 
+var ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+class App extends Component {
+    state = {
+        boardKeys: this.generateKeys()
+    };
+
+    generateKeys() {
+        var alphabet = ALPHABET.split('').reverse();
+        const result = [];
+        while (result.length < ALPHABET.length) {
+            const boardKey = alphabet.pop();
+            result.push(boardKey);
+        }
+        return result;
+    }
+
+    // Arrow fx for binding
+    handleBoardKeyClick = boardKey => {
+        console.log(boardKey + ' clicked');
+    };
+
+    render() {
+        const { boardKeys } = this.state;
+        return (
+            <div className="pendu">
+                {boardKeys.map((boardKey, index) => (
+                    <BoardKey key={index} boardKey={boardKey} feedback="unclicked" onClick={this.handleBoardKeyClick} />
+                ))}
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/BoardKey.js
+++ src/BoardKey.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import './BoardKey.css';
 
-const BoardKey = ({key, feedback, onClick}) => (
-  <div className={`key ${feedback}`} onClick={() => onClick(key)}>
-      <span className="key">{key}</span>
-  </div>
+const BoardKey = ({ boardKey, feedback, onClick }) => (
+    <div className={`key ${feedback}`} onClick={() => onClick(boardKey)}>
+        <span className="key">{boardKey}</span>
+    </div>
 );
+
+export default BoardKey;
